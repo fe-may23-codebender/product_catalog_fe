300689029ffbc4bd09889ef299f30cdbbef6a9d2
Marian Volytskyi <volmark00@gmail.com>
2023-08-28 15:17:47 +0300

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 type Props = {};
 
 export const App: FC<Props> = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/phones" element={<h1>Phone catalog page</h1>} />
+
+        <Route path="*" element={<p>Page not found</p>} />
+      </Routes>
+    </div>
+  );
 };
