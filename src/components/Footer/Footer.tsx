11d217f27833746e4757c857ee4e76ca15f69cff
Iryna <irinabolbat98@gmail.com>
2023-08-29 14:56:17 +0300

    fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { Logo } from '../Logo/Logo';
 import styles from './Footer.module.scss';
+import style from '../../styles/utils/container.module.scss';
 
 const footerLinks = [
   {
@@ -17,37 +18,39 @@ export const Footer = () => {
   }
 
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footer__logo}>
-        <Logo />
-      </div>
+    <div className={style.limit}>
+      <footer className={styles.footer}>
+        <div className={styles.footer__logo}>
+          <Logo />
+        </div>
 
-      <div className={styles.footer__nav}>
-        <ul className={styles.footer__nav__list}>
-          {footerLinks.map(link => (
-            <li key={link.title} className={styles.footer__nav__item}>
-              <NavLink
-                to={link.path}
-                target="_blank"
-                className={styles.footer__nav__link}
-              >
-                {link.title}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div className={styles.footer__nav}>
+          <ul className={styles.footer__nav__list}>
+            {footerLinks.map(link => (
+              <li key={link.title} className={styles.footer__nav__item}>
+                <NavLink
+                  to={link.path}
+                  target="_blank"
+                  className={styles.footer__nav__link}
+                >
+                  {link.title}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
 
-      <div className={styles.footer__button}>
-        <span className={styles.footer__button__text}>Back to top</span>
-        <button
-          type="button"
-          className={styles.button}
-          onClick={goUp}
-        >
-          <div className={styles.button__up} />
-        </button>
-      </div>
-    </footer>
+        <div className={styles.footer__button}>
+          <span className={styles.footer__button__text}>Back to top</span>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={goUp}
+          >
+            <div className={styles.button__up} />
+          </button>
+        </div>
+      </footer>
+    </div>
   );
 };
