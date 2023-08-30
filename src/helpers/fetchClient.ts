9b942fe0e25fed0edd2a70b0f5175a141d54d013
Oleksii Lavka <lavka.alexey37gmail.com>
2023-08-30 15:37:04 +0300

    added redux store

--- src/helpers/fetchClient.ts
+++ src/helpers/fetchClient.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-const BASE_URL = '';
+const BASE_URL = 'https://codebender-catalog.onrender.com';
 
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
---
+++ src/redux/slices/productDetailsSlice.ts
@@ -0,0 +1,64 @@
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { getProductDescription, getProductInfo } from '../../api/products';
+import {
+  ProductDetails,
+  ProductCategory,
+} from '../../types';
+
+export interface ProductDetailsState {
+  item: ProductDetails | null;
+  loaded: boolean;
+  hasError: boolean;
+}
+
+type ProductParams = {
+  productCategory: ProductCategory;
+  productId: string;
+};
+
+const initialState: ProductDetailsState = {
+  item: null,
+  loaded: false,
+  hasError: false,
+};
+
+export const fecthProductDetails = createAsyncThunk(
+  'productDetails/fetch',
+  ({ productCategory, productId }: ProductParams) => {
+    return Promise.all([
+      getProductInfo(productCategory, productId),
+      getProductDescription(productId),
+    ]);
+  },
+);
+
+export const productDetailsSlice = createSlice({
+  name: 'productDetails',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fecthProductDetails.pending, (state) => {
+        state.loaded = false;
+        state.hasError = false;
+      })
+      .addCase(fecthProductDetails.fulfilled, (state, action) => {
+        const [productInfo, productDescription] = action.payload;
+
+        state.item = {
+          ...productInfo,
+          description: productDescription,
+        };
+
+        state.hasError = false;
+        state.loaded = true;
+      })
+      .addCase(fecthProductDetails.rejected, (state) => {
+        state.loaded = true;
+        state.hasError = true;
+      });
+  },
+});
+
+export default productDetailsSlice.reducer;
