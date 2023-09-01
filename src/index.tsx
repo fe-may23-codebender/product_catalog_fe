e0ab49e69db33fe76c72685af226b8c9a8bdcc97
Oleksii Lavka <lavka.alexey37gmail.com>
2023-09-01 10:15:53 +0300

    added hash router for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { App } from './App';
 import { store } from './redux/store';
 import './styles/index.scss';
