f7bd3cecc0401d8ce8269c0587b8d788fe0fb73b
Oleksii Lavka <lavka.alexey37gmail.com>
2023-09-05 17:03:22 +0300

    fixed carousel styles

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,15 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation, Pagination } from 'swiper';
+import { Navigation, Pagination, Autoplay } from 'swiper';
+import cn from 'classnames';
+import { useBreakpoint } from '../../hooks/useBreakpoint';
+import { Breakpoint, ButtonType } from '../../types';
+import { Button } from '../Buttons/Button';
+
+import 'swiper/swiper-bundle.min.css';
+import styles from './Carousel.module.scss';
+
+import arrowLeft from '../../assets/icons/black-arrows/arrow-left.svg';
+import arrowRight from '../../assets/icons/black-arrows/arrow-right.svg';
 
 import Baner1 from '../../assets/banners/banner-phones.png';
 import Baner2 from '../../assets/banners/banner-tablets.png';
@@ -7,19 +17,7 @@ import Baner3 from '../../assets/banners/banner-iphone14.png';
 import Baner4 from '../../assets/banners/banner-accessories.png';
 import Baner5 from '../../assets/banners/banner-iphone14-small.png';
 
-import 'swiper/css';
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import styles from './Carousel.module.scss';
-import { useBreakpoint } from '../../hooks/useBreakpoint';
-import { Breakpoint, ButtonType } from '../../types';
-import { Button } from '../Buttons/Button';
-import arrowLeft from '../../assets/icons/black-arrows/arrow-left.svg';
-import arrowRight from '../../assets/icons/black-arrows/arrow-right.svg';
-
-SwiperCore.use([Navigation, Pagination]);
-
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 let swiperInstance: any = null;
 
 const baners = [Baner1, Baner2, Baner4];
@@ -42,39 +40,58 @@ export const Carousel = () => {
   const banerCheck = breakpoint === Breakpoint.Mobile ? Baner5 : Baner3;
 
   return (
-    <div className={styles.slider}>
+    <div className={styles.carousel}>
       <Button
         type={ButtonType.Button}
-        className={styles.slider__btn}
+        className={styles.carousel__btn}
         onClick={goPrev}
         iconPath={arrowLeft}
       />
 
       <Swiper
+        className={styles.content}
+        wrapperClass={styles.wrapper}
         onSwiper={(swiper) => {
           swiperInstance = swiper;
         }}
-        modules={[Navigation, Pagination]}
+        modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={20}
         slidesPerView={1}
-        loop
         pagination={{
           clickable: true,
+          el: `.${styles.pagination}`,
+        }}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop
+        speed={600}
+        breakpoints={{
+          [Breakpoint.Mobile]: {
+            allowTouchMove: true,
+          },
+          [Breakpoint.Desktop]: {
+            allowTouchMove: false,
+          },
         }}
       >
-        <SwiperSlide className={styles.slide}>
-          <img className={styles.slider__img} src={banerCheck} alt="baner 1" />
+        <SwiperSlide className={cn(styles.slide, styles.nextBtn)}>
+          <img className={styles.slide__img} src={banerCheck} alt="baner 1" />
         </SwiperSlide>
+
         {baners.map((baner) => (
           <SwiperSlide key={baner} className={styles.slide}>
-            <img className={styles.slider__img} src={baner} alt="baner 1" />
+            <img className={styles.slide__img} src={baner} alt="baner 1" />
           </SwiperSlide>
         ))}
+
+        <div className={styles.pagination} />
       </Swiper>
 
       <Button
         type={ButtonType.Button}
-        className={styles.slider__btn}
+        className={cn(styles.carousel__btn, styles.prevBtn)}
         onClick={goNext}
         iconPath={arrowRight}
       />
